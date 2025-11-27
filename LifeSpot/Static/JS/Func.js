5452dbc8cabd0cb679aca1c1a6083aaf328fc2ca
Zoom7122 <v.lyubchencko@gmail.com>
2025-11-27 22:49:31 +0300

    1212

---
+++ LifeSpot/Static/JS/Func.js
@@ -0,0 +1,22 @@
+﻿
+function FindStrem(userInput) {
+
+    // Пробегаемся по контейнерам
+    let elements = document.getElementsByClassName('video-container');
+
+    // Пробегаемся по контейнерам
+    for (let i = 0; i < elements.length; i++) {
+        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
+        let videoText = elements[i].querySelector(".video-title").innerText;
+
+
+        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
+            // Скрываем неподходящие
+            elements[i].style.display = 'none';
+        } else {
+            // Показываем подходящие
+            elements[i].style.display = 'inline-block';
+        }
+    }
+}
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,47 @@
+﻿
+
+function indexFunc() {
+    let age = prompt("Пожалуйста, введите ваш возраст");
+    if (age >= 18) {
+        alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+setTimeout(() =>
+    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
+    30000);
+
+
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
+
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
